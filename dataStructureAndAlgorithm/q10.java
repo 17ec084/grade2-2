38841f6888aa1e50c0bd40840d420d4bf16011e0
Tomotaka_Hirata <17ec084@ms.dendai.ac.jp>
2018-11-27 19:25:10 +0900

    Create q10.java
    問題10のjavaプログラム

---
+++ dataStructureAndAlgorithm/q10.java
@@ -0,0 +1,26 @@
+﻿class q10
+//文字列の中から特定の文字を探し出すアルゴリズム
+{
+    static int strpos(String haystack, char needle)
+    //乾草(haystack)の中から針(needle)を探すメソッド
+    {
+        //char[] arr = haystack.toCharArray();
+        
+        for(int i=0; ; i++)
+        {
+            if(i==haystack.length())
+                return -1;
+            if(haystack.toCharArray()[i]==needle)
+          //if(arr[i]==needle)
+                return i;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        String univ_name = "TokyoDenkiUniversity";
+        char key = 'k';
+        System.out.println(strpos(univ_name,key));
+    }
+
+}
